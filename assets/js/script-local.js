fcdd7496668032c0cc4bde1e8e7d95b6f155151e
Pradana Yoga <pradanay64@gmail.com>
2023-11-16 12:17:21 +0700

    Sibar Menu dan Halaman Content sudah jadi

--- assets/js/script-local.js
+++ assets/js/script-local.js
@@ -2,5 +2,6 @@ const sidebar = document.querySelector(".sidebar");
 const toggle = document.querySelector(".toggle");
 toggle.addEventListener("click", () =>{
     sidebar.classList.toggle("side-min");
-    // console.log("Berhasil di Klik");
+    toggle.classList.toggle("active");
+    console.log("Berhasil di Klik");
 })
